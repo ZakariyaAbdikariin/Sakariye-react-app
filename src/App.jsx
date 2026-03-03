d4f8018db52227e599b5dba635222559f958fcb9
ZakariyaAbdikariin <sak.abdikariin@gmail.com>
2026-03-03 15:40:59 +0000

    I added a Counter component using useState with increase and decrease buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
+import Counter from "./components/Counter";
 import Greeting from "./components/Greeting";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <Greeting name="Sakariye" surname="Abdikariin" />
-      <Header />
-      <Header />
-      <Header />
+
+      <Counter />
     </>
   );
 }
--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -2,12 +2,12 @@ import { Fragment } from "react";
 
 function Greeting(props) {
   return (
-    <>
+    <div style={{ backgroundColor: "black", color: "white" }}>
       <h1>
         Hello, {props.name} {props.surname}!
       </h1>
       <h2>This text was not there originally</h2>
-    </>
+    </div>
   );
 }
 
