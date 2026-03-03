b081d698c16aa55b44c177b4bef0d907aa7b283a
ZakariyaAbdikariin <sak.abdikariin@gmail.com>
2026-03-03 14:13:12 +0000

    I am adding my react app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Greeting from "./components/Greeting";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Greeting name="Sakariye" surname="Abdikariin" />
+      <Header />
+      <Header />
+      <Header />
+    </>
+  );
+}
+
+export default App;
